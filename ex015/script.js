f51cd1ab9766a85c5f0e4695041587641175223c
JAPA-DARK <125380994+JAPA-DARK@users.noreply.github.com>
2024-10-06 23:22:56 -0300

    finalizando exercicio de JS

--- ex015/script.js
+++ ex015/script.js
@@ -1,3 +1,54 @@
 function verificar() {
-    window.alert('funcionou!')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('erro verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+
+    if (fsex[0].checked) {
+            gênero = 'homem'
+
+                if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'criança-menino.jpg')
+    
+                } else if (idade > 8 && idade < 18) {
+                    img.setAttribute('src', 'jovem-homem.jpg')
+    
+                } else if (idade > 17 && idade < 65) {
+                    img.setAttribute('src', 'adulto-homem.jpg')
+    
+                } else (idade >=65) {
+                    img.setAttribute('src', 'idoso-homem.jpg')
+                }
+
+    } else if (fsex[1].checked) {
+            gênero = 'mulher'
+
+            if(idade >=0 && idade < 10) {
+                //criança
+
+                } else if (idade > 8 && idade < 18) {
+                    //jovem
+
+                } else if (idade >17 && idade < 65) {
+                    //adulto
+
+                } else  (idade >=65) {
+                    //idoso
+
+                }
+
+            var img = document.createElement('img')
+            img.setAttribute('id', 'foto')
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
+    }
+
 }
