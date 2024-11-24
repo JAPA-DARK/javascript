26fff1ae0df271dcdad318ce3b391b1ef8004d21
JAPA-DARK <125380994+JAPA-DARK@users.noreply.github.com>
2024-11-23 22:44:18 -0300

    aula 14 finalizada

---
+++ aula13/ambientes.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log(`tudo bem?`)
+    c++
+} while(c <= 5)
+
+
+/* 
+    var c = 1
+    while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+*/
+
+
+
+/*  console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?')
+    console.log('tudo bem?') */
